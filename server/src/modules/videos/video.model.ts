6e63e1330a7f569dfa4dc56a7c6e9d6e2d529979
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-15 23:10:03 +0530

    added video models

---
+++ server/src/modules/videos/video.model.ts
@@ -0,0 +1,30 @@
+import { prop, getModelForClass, Ref } from "@typegoose/typegoose";
+import { User } from "../user/user.model";
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789",10);
+export class Video {
+  @prop()
+  public title: string;
+
+  @prop()
+  public description: string;
+
+  @prop({ enum: ["mp4"] })
+  public extension: string;
+
+  @prop({ required: true, ref: () => User })
+  public owner: Ref<User>;
+
+  @prop({ unique: true, default: () => nanoid() })
+  public videoId: string;
+
+  @prop({ default: false })
+  public published: boolean;
+}
+
+export const VideoModel = getModelForClass(Video, {
+  schemaOptions: {
+    timestamps: true,
+  },
+});
