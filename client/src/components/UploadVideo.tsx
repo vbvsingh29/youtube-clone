8bdff11075e3ec6aab1958209fb3e0d16d3f9ce2
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-18 14:47:42 +0530

    upload video stepper modal + input border

--- client/src/components/UploadVideo.tsx
+++ client/src/components/UploadVideo.tsx
@@ -1,11 +1,12 @@
-import { Modal, Button, Form, ProgressBar } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { updateVideo, uploadVideo } from "../../api/index";
-import { AxiosError, AxiosResponse } from "axios";
 import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Video } from "../../types";
 import { useVideo } from "../../context/videos";
+import { Slide, toast } from "react-toastify";
+import Progress_Bar from "../utils/ProgressBar";
 
 function EditVideoForm({ videoId, onClose, videos }) {
   const { register, handleSubmit } = useForm();
@@ -22,8 +23,26 @@ function EditVideoForm({ videoId, onClose, videos }) {
       });
       onClose();
       // videos();
+      toast.success("Video published Successfully", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
     } catch (error) {
       console.error("Error updating video:", error);
+      toast.error("Error, please try again later", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
     }
   };
 
@@ -45,7 +64,7 @@ function EditVideoForm({ videoId, onClose, videos }) {
           type="text"
           id="title"
           {...register("title", { required: true })}
-          className="border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800  p-2"
         />
       </div>
       <div className="mb-4">
@@ -58,7 +77,7 @@ function EditVideoForm({ videoId, onClose, videos }) {
         <textarea
           id="description"
           {...register("description", { required: true })}
-          className="border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800  p-2"
         />
       </div>
       <div className="mb-4">
@@ -70,7 +89,7 @@ function EditVideoForm({ videoId, onClose, videos }) {
           id="thumbnail"
           {...register("thumbnail")}
           onChange={handleThumbnailChange}
-          className="border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500 block w-full text-gray-800  p-2"
         />
       </div>
       <div className="mb-4">
@@ -129,9 +148,11 @@ function CustomModal({ show, onClose, children, progress }) {
                   </button>
                 </div>
                 {children}
-                {progress > 0 && (
-                  <ProgressBar now={progress} label={`${progress}%`} animated />
-                )}
+                {/* {progress > 0 && (
+                  <div className="mt-4">
+                    <Progress_Bar progressPercentage={progress} />
+                  </div>
+                )} */}
               </div>
             </div>
           </div>
@@ -146,7 +167,7 @@ function UploadVideo() {
   const [progress, setProgress] = useState(0);
   const [video, setVideo] = useState<Video>(null);
   const videos = useVideo();
-  console.log(progress, "PROGRES", video);
+
   const handleUpload = async (acceptedFiles) => {
     const formData = new FormData();
     formData.append("video", acceptedFiles[0]);
@@ -162,11 +183,30 @@ function UploadVideo() {
           },
         },
       });
-      console.log(response, "RESPONSE");
       setVideo(response);
-      setShow(false);
+      setProgress(100);
+      setShow(true);
+      toast.success("Video uploaded Successfully", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
     } catch (error) {
       console.error("Error uploading video:", error);
+      setProgress(0);
+      toast.error("Error uploading video. Please try again.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
     }
   };
 
@@ -180,55 +220,58 @@ function UploadVideo() {
       </button>
       <CustomModal
         show={show}
-        onClose={() => setShow(false)}
+        onClose={() => {
+          setProgress(0);
+          setShow(false);
+        }}
         progress={progress}
       >
-        <div>
-          {progress === 0 && (
-            <Dropzone onDrop={handleUpload}>
-              {({ getRootProps, getInputProps }) => (
-                <section>
-                  <div
-                    {...getRootProps()}
-                    className="w-full rounded-md cursor-pointer h-80 border-4 border-dashed border-gray-600 flex items-center justify-center hover:border-blue-500 focus:outline-none"
-                  >
-                    <input {...getInputProps()} />
-                    <div className="text-center">
-                      <div className="flex justify-center mb-2">
-                        <svg
-                          className="h-12 w-12 text-gray-600"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                          />
-                        </svg>
-                      </div>
-                      <p className="text-gray-600">
-                        Drag 'n' drop some files here, or click to select files
-                      </p>
+        {progress === 0 && (
+          <Dropzone onDrop={handleUpload}>
+            {({ getRootProps, getInputProps }) => (
+              <section>
+                <div
+                  {...getRootProps()}
+                  className="w-full rounded-md cursor-pointer h-80 border-4 border-dashed border-gray-600 flex items-center justify-center hover:border-blue-500 focus:outline-none"
+                >
+                  <input {...getInputProps()} />
+                  <div className="text-center">
+                    <div className="flex justify-center mb-2">
+                      <svg
+                        className="h-12 w-12 text-gray-600"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                        />
+                      </svg>
                     </div>
+                    <p className="text-gray-600">
+                      Drag 'n' drop some files here, or click to select files
+                    </p>
                   </div>
-                </section>
-              )}
-            </Dropzone>
-          )}
-          {progress > 0 && (
-            <ProgressBar now={progress} label={`${progress}%`} animated />
-          )}
-          {progress === 100 && (
-            <EditVideoForm
-              videoId={video?.videoId}
-              onClose={() => setShow(false)}
-              videos={videos}
-            />
-          )}
-        </div>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        )}
+        {progress > 0 && progress < 100 && (
+          <div className="mt-4">
+            <Progress_Bar progressPercentage={progress} />
+          </div>
+        )}
+        {progress === 100 && (
+          <EditVideoForm
+            videoId={video?.videoId}
+            onClose={() => setShow(false)}
+            videos={videos}
+          />
+        )}
       </CustomModal>
     </>
   );
