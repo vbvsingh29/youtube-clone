aa5f61ba391a9e708650b6b2f92d0819bae57489
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-18 21:06:09 +0530

    configuration aws client side

---
+++ client/aws/aws.config.ts
@@ -0,0 +1,17 @@
+//@ts-ignore
+import aws from "aws-sdk";
+
+import {
+  AWS_ACCESS_KEY_ID,
+  AWS_REGION,
+  AWS_SECRET_ACCESS_KEY,
+} from "../src/utils/constants";
+
+aws.config.update({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: AWS_REGION,
+});
+
+const s3 = new aws.S3();
+export default s3;
