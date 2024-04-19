023ae19b58691e4f851ff5958632426780a5ecb1
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-19 14:24:53 +0530

    cookie modification v3

--- client/api/axios.config.ts
+++ client/api/axios.config.ts
@@ -3,6 +3,7 @@ import { API_ENDPOINT } from "../src/utils/constants";
 
 const axiosInstance = axios.create({
   baseURL: API_ENDPOINT,
+  withCredentials: true,
 });
 
 export default axiosInstance;
