e0eff29fc8d4fd3a01768fc45a84ad43cf4b377f
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-19 18:12:22 +0530

    cookie modification v7

--- server/src/main.ts
+++ server/src/main.ts
@@ -28,6 +28,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
+  const origin = CORS_ORIGIN || 'https://youtube-clone-1-zebi.onrender.com';
   res.setHeader("Access-Control-Allow-Origin", CORS_ORIGIN || "");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
