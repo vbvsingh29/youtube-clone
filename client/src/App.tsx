472c165369863a14756d22c9c1a9d6f0dc6b4ec6
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-21 20:13:38 +0530

    added error page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import Footer from "./layout/Footer";
 import Home from "./pages/Home";
 import Header from "./layout/Header";
@@ -11,6 +10,8 @@ import { MeContextProvider } from "../context/me";
 import { VideoContextProvider } from "../context/videos";
 import WatchVideo from "./pages/watch/WatchVideo";
 import About from "./pages/About";
+import AuthorIntro from "./pages/AuthotInto";
+import ErrorPage from "./pages/ErrorPage";
 
 const App = () => (
   <BrowserRouter>
@@ -19,12 +20,14 @@ const App = () => (
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/author" element={<AuthorIntro />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
           <Route path="/watch/:query" element={<WatchVideo />} />
+          <Route path="*" element={<ErrorPage />} /> 
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
         <ToastContainer
           position="top-center"
           hideProgressBar
